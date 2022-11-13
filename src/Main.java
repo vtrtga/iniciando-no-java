c6fb720bdad5455403ad5204d30053171d84f03b
vtrtga <vitor.ypvalim@gmail.com>
2022-11-13 15:12:48 -0400

    Calculadora

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Estou iniciando meus aprendizados em Java!");
+	
+	static public void main (String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	Numero n1 = new Numero();
+	Numero n2 = new Numero();
+	Numero res = new Numero();
+	
+	System.out.printf("%nDigite o valor 1: ");
+	n1.setValor(scan.nextInt());
+	
+	System.out.printf("%nDigite o valor 2: ");
+	n2.setValor(scan.nextInt());
+	
+	res.setValor(n1.getValor() + n2.getValor());
+	
+	System.out.printf("%nA soma de %d com %d e igual a %d", n1.getValor(), n2.getValor(), res.getValor());
 	}
-
 }
