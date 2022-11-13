d178f8501b409962525a430a2cc9f0681c537a31
vtrtga <vitor.ypvalim@gmail.com>
2022-11-13 14:18:07 -0400

    Hello World!

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int idade = 25;
-		System.out.println("Olá Mundo!" + idade);
+		System.out.println("Estou iniciando meus aprendizados em Java!");
 	}
 
 }
